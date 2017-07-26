8b1ffdea327d01849706accfd1c6776dc4596c23
Sara Hester <shester@basecampcodingacademy.org>
2017-07-26 08:33:23 -0500

    Completed and moved a couple functions

--- disk.py
+++ disk.py
@@ -1,6 +1,13 @@
 #Opening and closing files goes here
 def open_log():
+    with open('history.txt', 'r') as file:
+        file.readline()
+        log = file.readlines()
+    return log
 def append_log():
 def open_inventory():
+    with open('inventory.txt', 'r') as file:
+        file.readline()
+        inventory = file.readlines()
+    return inventory
 def append_inventory():
-    
--- shell.py
+++ shell.py
@@ -2,12 +2,14 @@
 ##Import core and disk
 import core, disk
 ##make helper functions
+def make_pretty(inventory):
+    
+
+
 ##make main
-def print_cool_letters(letters):
-    blocks = map(core.find_cool_letters, letters)
-    print('\n'.join(map('\t'.join, zip(*blocks))))
 def main():
 print("WELCOME to Game-Flix!!")
+#Start Branch
 answer = input('Are you a 1.customer or 2.employee?')
 if answer == '1':
     #show inventory

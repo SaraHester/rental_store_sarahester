cd7dfb0eee8438be5630f33a63ce64d756a3e7b9
Sara Hester <shester@basecampcodingacademy.org>
2017-07-27 13:14:34 -0500

    Made function in test_core.py called test_make_log_dict
    Made the test function and fixed minor syntax errors . The function passed when pytest was run.
    
    Next, I'm going to create the test function for make_log_str

--- test_core.py
+++ test_core.py
@@ -14,3 +14,10 @@ def test_dict_ineven_to_str():
         3: {'quantity': 8, 'number': '3', 'price': 2.05, 'value': 24.0, 'name': 'Need for Speed:Most Wanted'}
         }
     assert core.dict_inven_to_str(inventory) == '\n1, Call of Duty, 3.0, -9, 35.0\n2, Deadman Wonderland(season 1), 5.0, -13, 45.0\n3, Need for Speed:Most Wanted, 2.05, 8, 24.0'
+def test_make_log_dict():
+    log = ['84742666, Deadman Wonderland(season 1), 2017-07-27 11:06:12.902951, N/A, N/A\n', '04470289, Call of Duty, 2017-07-27 11:06:43.142199, 2017-07-27 11:07:29.249265, 16.295\n', '44030379, Call of Duty, 2017-07-27 11:06:18.614966, N/A, N/A']
+    assert core.make_log_dict(log) == {
+        '04470289': {'time checked out': '2017-07-27 11:06:43.142199', 'total': '16.295\n', 'name': 'Call of Duty', 'id': '04470289', 'time checked in': '2017-07-27 11:07:29.249265'}, 
+        '44030379': {'time checked out': '2017-07-27 11:06:18.614966', 'total': 'N/A', 'name': 'Call of Duty', 'id': '44030379', 'time checked in': 'N/A'}, 
+        '84742666': {'time checked out': '2017-07-27 11:06:12.902951', 'total': 'N/A\n', 'name': 'Deadman Wonderland(season 1)', 'id': '84742666', 'time checked in': 'N/A'}
+        }

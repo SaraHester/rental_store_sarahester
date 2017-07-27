3b41feeffad046d62596242166aeb19ef6f29e12
Sara Hester <shester@basecampcodingacademy.org>
2017-07-27 12:47:49 -0500

    Fixed test_dict_inven_to_str
    I added a couple \n charachters on the right of the assert statement.
    
    Next,  I am going to investigate a possible bug in the inventory where the quantity  is negitive

--- test_core.py
+++ test_core.py
@@ -13,4 +13,4 @@ def test_dict_ineven_to_str():
         2: {'quantity': -13, 'number': '2', 'price': 5.0, 'value': 45.0, 'name': 'Deadman Wonderland(season 1)'},
         3: {'quantity': 8, 'number': '3', 'price': 2.05, 'value': 24.0, 'name': 'Need for Speed:Most Wanted'}
         }
-    assert core.dict_inven_to_str(inventory) == '1, Call of Duty, 3.0, -9, 35.0\n' + '2, Deadman Wonderland(season 1), 5.0, -13, 45.0\n' + '3, Need for Speed:Most Wanted, 2.05, 8, 24.0'
+    assert core.dict_inven_to_str(inventory) == '\n1, Call of Duty, 3.0, -9, 35.0\n2, Deadman Wonderland(season 1), 5.0, -13, 45.0\n3, Need for Speed:Most Wanted, 2.05, 8, 24.0'
